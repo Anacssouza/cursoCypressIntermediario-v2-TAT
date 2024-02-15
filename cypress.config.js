749413563397caa00a59a1c3694e3c0412f1f5c8
ana <anacsilveirasouza@gmail.com>
2024-02-14 22:11:33 -0300

    seção 11

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,7 @@ module.exports = defineConfig({
       hideCredentials: true, // o token de acesso (o qual é um dados sensível) está protegido ao rodar o teste
       requestMode: true,
     },
+    experimentalRunAllSpecs: true, //pra poder rodar todos os testes de uma vez só no modo interativo
   },
   fixturesFolder: false, // pra não criar a pasta fixtures quando rodar pq não vai ser utilizada no curso
   video: false,          // pra não criar videos quando realizar os testes
